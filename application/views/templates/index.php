48a4ae0e88c2b7a3c761ea241ef17cbbd4baf287
Mohamad Rizky Reynaldy <rizkytsun@gmail.com>
2020-12-17 21:27:25 +0700

    Integrate Bootstrap Gentelella

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -10,21 +10,21 @@
 <link rel="icon" href="images/favicon.ico" type="image/ico" />
 <title>Gentelella Alela! | </title>
 
-<link href="<?php echo base_url() ?>gentelella/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<link href="<?php echo base_url() ?>gentelella/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
-<link href="<?php echo base_url() ?>gentelella/nprogress/nprogress.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/nprogress/nprogress.css" rel="stylesheet">
 
-<link href="<?php echo base_url() ?>gentelella/iCheck/skins/flat/green.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
 
-<link href="<?php echo base_url() ?>gentelella/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 
-<link href="<?php echo base_url() ?>gentelella/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
 
-<link href="<?php echo base_url() ?>gentelella/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
-<link href="<?php echo base_url() ?>gentelella/build/css/custom.min.css" rel="stylesheet">
+<link href="<?php echo base_url() ?>assets/gentelella/build/css/custom.min.css" rel="stylesheet">
 </head>
 <body class="nav-md">
 <div class="container body">
@@ -916,43 +916,43 @@ Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib
 </div>
 </div>
 
-<script src="<?php echo base_url() ?>gentelella/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/jquery/dist/jquery.min.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/fastclick/lib/fastclick.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/nprogress/nprogress.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/nprogress/nprogress.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/Chart.js/dist/Chart.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/Chart.js/dist/Chart.min.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/gauge.js/dist/gauge.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/gauge.js/dist/gauge.min.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/iCheck/icheck.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/iCheck/icheck.min.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/skycons/skycons.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/skycons/skycons.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/Flot/jquery.flot.js"></script>
-<script src="<?php echo base_url() ?>gentelella/Flot/jquery.flot.pie.js"></script>
-<script src="<?php echo base_url() ?>gentelella/Flot/jquery.flot.time.js"></script>
-<script src="<?php echo base_url() ?>gentelella/Flot/jquery.flot.stack.js"></script>
-<script src="<?php echo base_url() ?>gentelella/Flot/jquery.flot.resize.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/Flot/jquery.flot.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/Flot/jquery.flot.pie.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/Flot/jquery.flot.time.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/Flot/jquery.flot.stack.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/Flot/jquery.flot.resize.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-<script src="<?php echo base_url() ?>gentelella/flot-spline/js/jquery.flot.spline.min.js"></script>
-<script src="<?php echo base_url() ?>gentelella/flot.curvedlines/curvedLines.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/flot.curvedlines/curvedLines.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/DateJS/build/date.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/DateJS/build/date.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/jqvmap/dist/jquery.vmap.js"></script>
-<script src="<?php echo base_url() ?>gentelella/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-<script src="<?php echo base_url() ?>gentelella/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/moment/min/moment.min.js"></script>
-<script src="<?php echo base_url() ?>gentelella/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/moment/min/moment.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
 
-<script src="<?php echo base_url() ?>gentelella/build/js/custom.min.js"></script>
+<script src="<?php echo base_url() ?>assets/gentelella/build/js/custom.min.js"></script>
 </body>
 </html>
